ab3247d6f00b75ae58670474481fa47b8e0d0d98
RifkiEr24 <rifkierlangga17@gmail.com>
2021-07-10 17:29:07 +0700

    add vuex

--- src/main.js
+++ src/main.js
@@ -4,9 +4,12 @@ import VueAxios from 'vue-axios'
 import App from "./App.vue";
 import router from "./router";
 import "./scss/main.scss";
+import store from './store'
+
 Vue.use(VueAxios, axios)
 
 new Vue({
   router,
+  store,
   render: (h) => h(App),
 }).$mount("#app");
